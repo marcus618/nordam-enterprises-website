fb1174b29be0edbbd3b296d98d172de4e1252474
Marcus Mofjeld <au717932@uni.au.dk>
2025-08-07 12:41:29 +0200

    Organisation schema markup added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.nordamenterprises.com',
   integrations: [
     tailwind(),
   ]
