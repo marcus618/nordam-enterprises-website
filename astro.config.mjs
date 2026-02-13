79b9a50646c544fa0191dead93bd5613701dc22e
Marcus Mofjeld <au717932@uni.au.dk>
2026-02-13 13:22:10 +0100

    added vercel.json for envvar

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,11 +16,5 @@ export default defineConfig({
   ],
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }  }),
-    
-  vite: {
-    define: {
-      'process.env.KEYSTATIC_URL': JSON.stringify(process.env.KEYSTATIC_URL || 'https://www.nordamenterprises.com'),
-    }
-  }
+    webAnalytics: { enabled: true }  })
 });
