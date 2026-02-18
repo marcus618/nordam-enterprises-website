95077ebbb0bd1c91c1c2f8b0496c1cfb756d891a
Marcus Mofjeld <au717932@uni.au.dk>
2026-02-18 10:19:05 +0100

    first configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.nordamenterprises.com',
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind(),
   ]
