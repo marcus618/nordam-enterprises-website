021aea889a8600c9c37117805187573652bf3846
Marcus Mofjeld <au717932@uni.au.dk>
2026-02-25 20:48:34 +0100

    ready for prod?

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,10 +1,11 @@
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async ({ redirect }) => {
+export const GET: APIRoute = async ({ url, redirect }) => {
   const clientId = import.meta.env.GITHUB_CLIENT_ID;
-  // We hardcode localhost for now to test. Later we will make this dynamic.
-  const redirectUri = "http://localhost:4321/api/auth/callback"; 
-  const scope = "repo,user"; // 'repo' gives read/write access
+  const scope = "repo,user"; 
+  
+  // dynamically construct the redirect_uri based on the current URL (works in dev and prod)
+  const redirectUri = `${url.origin}/api/auth/callback`; 
 
   return redirect(
     `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`
